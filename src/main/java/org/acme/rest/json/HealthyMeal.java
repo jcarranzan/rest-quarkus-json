6d9b3f769fcf5ff4fe87b017b0e41ed5e2dd9858
Jose Carranza <jcarranz@redhat.com>
2023-08-10 16:28:35 +0200

    create HealthyMeal

---
+++ src/main/java/org/acme/rest/json/HealthyMeal.java
@@ -0,0 +1,15 @@
+package org.acme.rest.json;
+
+public class HealthyMeal {
+
+  public String name;
+  public String description;
+  public HealthyMeal() {
+  }
+
+  public HealthyMeal(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+
+}
